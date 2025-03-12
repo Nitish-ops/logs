dd7db750ea57260d7a89dcf40bc87fd4d3cfb3fd
Nitish-ops <nitish010128@gmail.com>
2025-03-12 14:36:23 +0530

    Update database.py

--- scheduler_logs/database.py
+++ scheduler_logs/database.py
@@ -3,17 +3,17 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.sql import func
 
-# REPLACE WITH YOUR ACTUAL CREDENTIALS!
+
 DATABASE_URL = 'mysql+pymysql://root:NiGr%408120@localhost/scheduler'
 
 Base = declarative_base()
 
 
 class Log(Base):
-    __tablename__ = 'logs'  # Corrected: Use __tablename__
+    __tablename__ = 'logs'  
     id = Column(Integer, primary_key=True)
     timestamp = Column(DateTime(timezone=True), server_default=func.now())
-    log_type = Column(String(50))  # Added log_type column
+    log_type = Column(String(50))  
     log_message = Column(Text)
 
 engine = create_engine(DATABASE_URL)
