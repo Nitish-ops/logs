69d57fe5ff08c9e43b929e06b8a4a256175aaa57
Nitish-ops <nitish010128@gmail.com>
2025-03-12 14:41:31 +0530

    Update database.py

--- scheduler_logs/database.py
+++ scheduler_logs/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.sql import func
 
-
+# i have updated
 DATABASE_URL = 'mysql+pymysql://root:NiGr%408120@localhost/scheduler'
 
 Base = declarative_base()
